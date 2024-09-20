1b46a8e5f3dd349c70f0d020768435f603e076c3
Aazam <abuzarali1098@gmail.com>
2024-09-20 18:11:19 +0530

    files added

---
+++ qrgenerator.js
@@ -0,0 +1,12 @@
+let imgBox = document.getElementById("img-box");
+let qrImage = document.getElementById("qrImage");
+let QRText = document.getElementById("qrText");
+
+
+function generateQR(){
+    if(QRText.value.length>0){
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + QRText.value;
+        imgBox.classList.add("show-img");
+    }
+   
+}
