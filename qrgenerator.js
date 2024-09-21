a5d83f69601972915417a4e5bd2d3158e461dac1
Aazam <abuzarali1098@gmail.com>
2024-09-21 14:42:26 +0530

    Complete project

--- qrgenerator.js
+++ qrgenerator.js
@@ -1,6 +1,7 @@
 let imgBox = document.getElementById("img-box");
 let qrImage = document.getElementById("qrImage");
 let QRText = document.getElementById("qrText");
+let downloadQR = document.getElementById("downloadBtn");
 
 
 
@@ -8,6 +9,7 @@ function generateQR(){
     if(QRText.value.length>0){
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + QRText.value;
         imgBox.classList.add("show-img");
+        downloadQR.style.visibility = 'visible';
     }
     else{
         //QRText.placeholder = "Empty text";
@@ -17,4 +19,12 @@ function generateQR(){
         }, 800);
     }
    
+}
+
+function downloadQRFunc() {
+    // Create a temporary link to download the QR image
+    const link = document.createElement('a');
+    link.href = qrImage.src;
+    link.download = 'QR_Code.png'; // Set the download file name
+    link.click(); // Programmatically click the link to trigger download
 }
